ccf69e787245d1bfd6330b1418601cbdbd5012d0
“jwomack26” <j.womack26@tcu.edu>
2021-03-23 19:36:47 -0500

    Added Styling

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ function App() {
 
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
+  const [questions, setQuestions] = useState();
 
 
   const fetchCategories = async () => {
     console.log('this will fetch the categories');
-    let res = await fetch('http://localhost:3000/api/v1/categories');
+    let res = await fetch('http://localhost:3001/api/v1/categories');
     let data = await res.json();
     console.log(data);
     setCategories(data);
@@ -34,35 +35,73 @@ function App() {
   // }, [someStateVariable, var2])
 
 
+  const fetchQuestionsForCategory = async (id) =>{
+    console.log('fetch questions for this category id', id);
+    let res = await fetch('http://localhost:3001/api/v1/categories/${id}/questions');
+    let data = await res.json();
+    console.log(data);
+    setQuestions(data);
+    //setCategories(data);
+  };
+  
+  
+  const createNewQuestion = async () => {
+    console.log('create a question for the category id', selectedCategory)
+  };
+  
   return (
     <>
-      <div className="grid grid-cols-12">
+      <div className="Top" >
         <div className={'col-span-full border p-5'}>
-          <h1 className={'text-center text-3xl'}>FORUM APP</h1>
+          <h1 className={'Heading'}>BILLIE EILISH</h1>
+          <p className={'SubHeading'}>FAN FORUM</p>
         </div>
 
       </div>
 
+      <div className="CategoryColumn">
       <div className="grid grid-cols-12">
         <div className={'col-span-full md:col-span-3 lg:col-span-2 border p-5'}>
-          {<h1 className={'text-center text-3xl'}>Categories list</h1>}
+          <div className="CategoryColumnHeading">
+          {<h1 className={'text-center text-xl p-5'}><b>SELECT A CATEGORY</b></h1>}
+          </div>
+            <div className="Categories">
             <ul>
+              
                 {categories.map((category, index) => {
                     return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
                         setSelectedCategory(category.id);
+                      fetchQuestionsForCategory(category.id)
                     }}>
                         {category.name}
                     </li>
+                  
                 })}
             </ul>
+            </div>
         </div>
+        
 
         <div className={'col-span-full md:col-span-9 lg:col-span-10 border p-5'}>
-          <h1 className={'text-center text-3xl'}>blank space</h1>
+          <div className="buttons">
+          <button className={'border p-2 pl-4 pr-4 bg-gray-200'} onClick={createNewQuestion}>New Question</button>
+          <br/>
+          <br/>
+          </div>
+          <ul>
+            {questions && questions.map((question) => {
+              return <li key={question.id}>
+                {question.question.Txt}
+              </li>
+
+            })}
+          </ul>
+
+          {/*questions && <p>{JSON.stringify(questions)}</p>*/}
         </div>
 
       </div>
-
+      </div>
       </>
   );
 }
